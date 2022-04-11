ab87bf4671e40b09a15b5c728f4f072c05a96175
Ales Rechtorik <foxxcz@gmail.com>
2022-04-11 14:59:48 +0200

    font import change

--- src/App.vue
+++ src/App.vue
@@ -83,6 +83,6 @@ export default defineComponent({
 </template>
 
 <style>
-@import url("https://fonts.googleapis.com/css?family=Roboto:400;500;600");
+@import url("https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;700&display=swap");
 @import "@zerops/zestratcss/zestrat.css";
 </style>
